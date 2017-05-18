88b418dd32ee2bab11512e87d00f49e3963d54f7
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-05-18 18:25:40 -0500

    adding body print for webhook post data

--- routes/webhooks.js
+++ routes/webhooks.js
@@ -4,12 +4,13 @@ var router = express.Router();
 var webhooks = ''
 /* GET users listing. */
 router.get('/', function(req, res) {
+  console.log(webhooks);
   res.send('respond with a resource');
 });
 router.post('/', function(req, res) {
 	webhooks = req.body
 	console.log(webhooks);
-	res.sendStatus(200);
+	res.sendStatus(200).send(webhooks);
 });
 
 module.exports = router;
