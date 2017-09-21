57dcad21359c235236559b79c214a9e522151c48
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-09-21 16:20:08 -0500

    moving webhooks var to global scope

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+const webhooks = []
 /* GET users listing. */
 router.post('/:hook', (req, res, next) => {
-	const webhooks = []
 	let body = JSON.stringify(req.body);
 	webhooks.push(body);
 	console.log('got ' + req.body + ' at ' + req.params.hook);
