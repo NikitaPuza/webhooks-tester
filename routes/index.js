a7a94d5c0d3d9d3f865e32cb527e54bbb5f9cb12
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2019-03-07 14:38:30 +1100

    fixing webhooks route

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,14 @@
 var express = require('express');
 var router = express.Router();
-/* GET users listing. */
-router.post('/:hook', (req, res, next) => {
-	const webhooks = '';
+
+
+const webhooks = '';
+
+router.post('/', (req, res) => {
 	webhooks = JSON.stringify(req.body);
-	res.locals.webhook = webhooks;
-	console.log('got ' + req.body + ' at ' + req.params.hook);
-	//res.sendStatus(200)
-  next()
+	res.sendStatus(200)
 });
-router.get('/:hook', function(req, res) {
-	let webhooks = req.locals.webhooks
+router.get('/', function(req, res) {
   res.render('index', {data: webhooks} );
 });
 
