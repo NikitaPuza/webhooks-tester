8d8383fe9137aef99e2069a1b4bdbb623c37977a
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2019-03-21 10:52:04 +1100

    adding cart endpoint

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', (req, res) => {
+app.post('/', (req, res,) => {
 	res.sendStatus(200);
 });
 app.use('/webhooks', index);
