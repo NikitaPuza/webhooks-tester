aad99d8ecd8baa663d8c8b32d88f6e64936cc409
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2019-03-21 09:56:57 +1100

    adding cart endpoint

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,21 @@ var router = express.Router();
 
 
 let webhooks = '';
+let converted = '';
 
 router.post('/hook', (req, res) => {
 	webhooks = JSON.stringify(req.body);
 	res.sendStatus(200)
 });
 router.get('/', function(req, res) {
-  res.render('index', {data: webhooks} );
+  res.render('index', {data: webhooks, converted} );
 });
 router.post('/cart', (req, res) => {
 	webhooks = JSON.stringify(req.body);
+	res.sendStatus(200)
+});
+router.post('/cart/converted', (req, res) => {
+	converted = JSON.stringify(req.body);
 	res.sendStatus(200)
 });
 module.exports = router;
