73f3077c33cd3c4172421910fe2e096a09d4230e
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2019-03-25 11:19:37 +1100

    combining hook bodies into single string before sending to fe

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@ let webhooks = '';
 let converted = '';
 
 router.post('/hook', (req, res) => {
-	webhooks = JSON.stringify(req.body);
+	webhooks =  webhooks + JSON.stringify(req.body);
 	res.sendStatus(200)
 });
 router.get('/', function(req, res) {
   res.render('index', {data: webhooks, converted} );
 });
 router.post('/cart', (req, res) => {
-	webhooks = JSON.stringify(req.body);
+	webhooks = webhooks + JSON.stringify(req.body);
 	res.sendStatus(200)
 });
 router.post('/cart/converted', (req, res) => {
-	converted = JSON.stringify(req.body);
+	converted =  converted + JSON.stringify(req.body);
 	res.sendStatus(200)
 });
 module.exports = router;
